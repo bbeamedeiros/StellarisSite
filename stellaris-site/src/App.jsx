a8868ca8991347b224791af0ea4fe128d672541a
bbeatrizmedeiros <beatrizbemedeiros@alu.ufc.br>
2025-06-27 16:05:24 -0300

    fazendo o footer

--- stellaris-site/src/App.jsx
+++ stellaris-site/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from './Componentes footer/Footer';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
