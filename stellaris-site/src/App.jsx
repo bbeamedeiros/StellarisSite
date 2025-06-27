340648bd5e310dd34e4c61170335d70e84bef12c
bbeatrizmedeiros <beatrizbemedeiros@alu.ufc.br>
2025-06-27 14:55:21 -0300

    algumas alterações na pasta de componentes

--- stellaris-site/src/App.jsx
+++ stellaris-site/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import Header from './Componentes padrão/Header'
+import { BrowserRouter as Roteador, Routes, Route } from 'react-router-dom';
 
 function App() {
-  
 
   return (
-    <>
-  
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaginaInicial />} />
+        <Route path='/produtos' element={<ListaProdutos />} />
+        <Route path="/parcerias" element={<PaginaParceiros />} />
+        <Route path="/eventos" element={<ListaEventos />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
