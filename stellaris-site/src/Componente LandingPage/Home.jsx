4656491ecbd8370a3f36961c9e2084dd2c4f5d9f
henrii089 <natanh2016@gmail.com>
2025-06-27 15:56:21 -0300

    coloquei as sections de eventos e produtos

--- stellaris-site/src/Componente LandingPage/Home.jsx
+++ stellaris-site/src/Componente LandingPage/Home.jsx
@@ -2,21 +2,26 @@ export default Home;
 import './Home.css'
 function Home() {
     return(
-    <div className="sobre-contato">
+        <div>
         <div className="sobre-nos">
             <section>
                 <h1>Sobre nós</h1>
                 <p>Nós somos a Stellaris, pppp</p>
             </section>
             </div>
-            <div className="contato">
-                <section>
-                    <h1>Contato</h1>
-                    <p>email@gmail.com</p>
-                    <p>888-888-8888</p>
-                </section>
-            </div>
-    </div>
+        <div className='eventos-stellaris'>
+            <section>
+                <h1>Eventos Stellaris</h1>
+                <p><h6>Tudo que é Stellaris, em um só lugar! Confira nossos eventos e viva a experiência completa.</h6></p>
+            </section>
+        </div>
+         <div className='produtos-stellaris'>
+            <section>
+                <h1>Produtos Stellaris</h1>
+                <p><h6>Conheça os produtos oficiais!</h6></p>
+            </section>
+        </div>
+</div>
     )
 }
 
