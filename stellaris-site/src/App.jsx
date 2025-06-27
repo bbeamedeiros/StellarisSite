ba40b2f7a0609ced462133e55bf1637b85496f4d
bbeatrizmedeiros <beatrizbemedeiros@alu.ufc.br>
2025-06-27 15:14:53 -0300

    blablabla

--- stellaris-site/src/App.jsx
+++ stellaris-site/src/App.jsx
@@ -1,22 +1,4 @@
 import './App.css'
-<<<<<<< HEAD
-import Header from './Componentes padrão/Header'
-import { BrowserRouter as Roteador, Routes, Route } from 'react-router-dom';
-
-function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PaginaInicial />} />
-        <Route path='/produtos' element={<ListaProdutos />} />
-        <Route path="/parcerias" element={<PaginaParceiros />} />
-        <Route path="/eventos" element={<ListaEventos />} />
-      </Routes>
-
-    </BrowserRouter>
-  )
-=======
 import Navbar from './components/Navbar';
 
 function App() {
@@ -25,7 +7,6 @@ function App() {
       <Navbar />
     </>
   );
->>>>>>> af1ca9f82515c6dc55ed8d7e84b18857b1594c50
 }
 
 export default App;
