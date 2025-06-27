73845162d376fe2ada15fcceb35f9895fd9ecae1
Lara Maria <lara.mariagurgel1611@gmail.com>
2025-06-27 15:21:21 -0300

    navbar

--- stellaris-site/src/components/Navbar/Navbar.jsx
+++ stellaris-site/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ function Navbar() {
   return (
     <nav className="nav">
       <ul className="menu">
+        <svg></svg>
         {navItems.map((item) => (
           <li key={item.href}>
             <a href={item.href}>{item.label}</a>
