c66d2e9a41aa1b9564ae9d2001098b2b1e971b45
Lara Maria <lara.mariagurgel1611@gmail.com>
2025-06-27 15:43:16 -0300

    logo na navbar

--- stellaris-site/src/components/Navbar/Navbar.jsx
+++ stellaris-site/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import './Navbar.css';
-import { ReactComponent as Logo } from '/src/assets/logohorizontal.svg';
+import logo from './logohorizontal.png';
 import { navItems } from './data';
 
 function Navbar() {
   return (
     <nav className="nav">
       <ul className="menu">
-        <li><Logo className="logo" /></li>
+        <li><img src={logo} alt="Logo" className="logo" /></li> {/* Use img */}
         {navItems.map((item) => (
           <li key={item.href}>
             <a href={item.href}>{item.label}</a>
