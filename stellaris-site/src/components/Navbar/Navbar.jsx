543b2c5239583d06146514e85acc2e766a417710
Lara Maria <lara.mariagurgel1611@gmail.com>
2025-06-27 15:25:15 -0300

    oi

--- stellaris-site/src/components/Navbar/Navbar.jsx
+++ stellaris-site/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import './Navbar.css';
+import { ReactComponent as Logo } from '/src/assets/logohorizontal.svg';
 import { navItems } from './data';
 
 function Navbar() {
   return (
     <nav className="nav">
       <ul className="menu">
-        <svg></svg>
+        <li><Logo className="logo" /></li>
         {navItems.map((item) => (
           <li key={item.href}>
             <a href={item.href}>{item.label}</a>
