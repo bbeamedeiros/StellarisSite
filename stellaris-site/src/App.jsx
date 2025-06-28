4fd8393bb65be332b618a86869d5789367a0c876
Lara Maria <lara.mariagurgel1611@gmail.com>
2025-06-28 11:35:38 -0300

    adicionando comentario qualquer

--- stellaris-site/src/App.jsx
+++ stellaris-site/src/App.jsx
@@ -3,6 +3,8 @@ import Footer from './Componentes footer/Footer';
 import Navbar from './components/Navbar';
 import Home from './Componente LandingPage/Home'
 
+// comentario
+
 function App() {
 
   return (
