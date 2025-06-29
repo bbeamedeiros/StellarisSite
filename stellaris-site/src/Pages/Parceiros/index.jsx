52abd5cc5b83ae101255f45eb3b3cc9e54b160ae
yasminalmeidaa <yasminalmeidaa>
2025-06-29 20:18:48 -0300

    quebradelinha

--- stellaris-site/src/Pages/Parceiros/index.jsx
+++ stellaris-site/src/Pages/Parceiros/index.jsx
@@ -1,7 +1,8 @@
 export default function Parceiros() {
   return (
     <div className="parceiros-container">
-      <h1>Parceiros</h1>
+      <h1>Vamos crescer juntos? 
+      Seja um parceiro da Stellaris!</h1>
       <p>Em breve mais informações sobre nossos parceiros.</p>
     </div>
   );
