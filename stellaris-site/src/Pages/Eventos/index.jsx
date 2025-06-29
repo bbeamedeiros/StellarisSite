7065b1fb6de13bcd0c85c87a6063658be4165a72
bbeatrizmedeiros <beatrizbemedeiros@alu.ufc.br>
2025-06-29 17:45:01 -0300

    fazendo o card

--- stellaris-site/src/Pages/Eventos/index.jsx
+++ stellaris-site/src/Pages/Eventos/index.jsx
@@ -1,12 +1,23 @@
+import CardEventos from '../../components/CardEventos';
 import './styles.css';
 
 export default function Eventos() {
+
+    const evento = {
+        titulo: "Próximo Evento",
+        descricao: "Em breve mais informações sobre o evento.",
+        imagem: "https://stellarisqxd.com/wp-content/uploads/2023/10/WhatsApp-Image-2023-10-16-at-11.22.08-AM.jpeg",
+        link: "#"
+    }
+
     return (
         <div className="eventos-container">
 
             <div className="evento-futuro">
                 <h1>Bora pro Próximo? Garanta o Seu Ingresso!</h1>
                 <p>Em breve mais informações sobre o evento.</p>
+                <CardEventos evento={evento} />
+
             </div>
 
             <div className="eventos-passados">
