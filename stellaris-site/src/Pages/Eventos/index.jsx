fad274b2c9c5d24369ceeaef303404ddb601c2a3
bbeatrizmedeiros <beatrizbemedeiros@alu.ufc.br>
2025-06-29 16:54:27 -0300

    page eventos

--- stellaris-site/src/Pages/Eventos/index.jsx
+++ stellaris-site/src/Pages/Eventos/index.jsx
@@ -1,13 +1,15 @@
+import './styles.css';
+
 export default function Eventos() {
     return (
         <div className="eventos-container">
 
-            <div className="eventos-futuro">
+            <div className="evento-futuro">
                 <h1>Bora pro Próximo? Garanta o Seu Ingresso!</h1>
                 <p>Em breve mais informações sobre o evento.</p>
             </div>
 
-            <div className="eventos-passado">
+            <div className="eventos-passados">
                 <h1>Momentos Inesquecíveis</h1>
                 <p>Só quem viveu sabe... Mas dá pra sentir o clima aqui.</p>
             </div>
