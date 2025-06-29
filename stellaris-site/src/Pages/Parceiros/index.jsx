dd55c1669ba20ce09801d9ec4cd15b74ddf0d722
yasminalmeidaa <yasminalmeidaa>
2025-06-29 20:30:13 -0300

    textplotemimpsi

--- stellaris-site/src/Pages/Parceiros/index.jsx
+++ stellaris-site/src/Pages/Parceiros/index.jsx
@@ -3,7 +3,13 @@ export default function Parceiros() {
     <div className="parceiros-container">
       <h1>Vamos crescer juntos? <p></p> 
       Seja um parceiro da Stellaris!</h1>
-      <p>Em breve mais informações sobre nossos parceiros.</p>
+      <pre>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Praesent ut malesuada nunc.
+        Curabitur vel lacus nec urna egestas aliquam.
+        Donec ac sem sed lorem commodo porttitor.
+      </pre>
+      <p></p>
     </div>
   );
 }
