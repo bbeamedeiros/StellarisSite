100a08a16e5f4a5a1a31f224da9c3fa17b935df0
bbeatrizmedeiros <beatrizbemedeiros@alu.ufc.br>
2025-06-29 23:03:06 -0300

    Fazendo ajustes na navbar

--- stellaris-site/src/Pages/Home/Home.jsx
+++ stellaris-site/src/Pages/Home/Home.jsx
@@ -9,59 +9,59 @@ import iconStar from '../../assets/iconStar.svg'
 import iconMsg from '../../assets/iconMsg.svg'
 
 function Home() {
-    return (
+    return(
         <div>
             <div className='header'>
                 <img src={header}></img>
             </div>
-            <div className="sobre-nos">
-                <div className='sobre-nos-texto'>
-                    <p className='title'>Quem somos <br /> na fila do rolê?</p>
-                    <p className='quem-somos'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quod laboriosam eligendi reiciendis eaque exercitationem! Quam ducimus cumque consequuntur in dolor officiis unde incidunt corrupti quas cupiditate? Labore, corporis alias.</p>
-                </div>
-                <div className='sobre-nos-imagem'>
-                    <img src={StellarisGrupo}></img>
-                </div>
+        <div className="sobre-nos">
+            <div className='sobre-nos-texto'>
+                <p className='title'>Quem somos <br/> na fila do rolê?</p>
+                <p className='quem-somos'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quod laboriosam eligendi reiciendis eaque exercitationem! Quam ducimus cumque consequuntur in dolor officiis unde incidunt corrupti quas cupiditate? Labore, corporis alias.</p>
             </div>
-            <div className='eventos-stellaris'>
+            <div className='sobre-nos-imagem'>
+            <img src={StellarisGrupo}></img>
+            </div>
+            </div>
+        <div className='eventos-stellaris'>
                 <p className='title'>Eventos Stellaris</p>
                 <p className='sub-title'>Tudo que é Stellaris, em um só lugar! Confira nossos eventos e viva a experiência completa.</p>
-                <BotaoBranco texto={"Explorar Eventos"} onClick={null} />
-            </div>
-            <div className='produtos-stellaris'>
-                <section>
-                    <p className='title'>Produtos Stellaris</p>
-                    <p className='sub-title'>Conheça os produtos oficiais!</p>
-                    <BotaoBranco texto={"Ver catálogo completo"} onClick={null} />
-                </section>
-            </div>
-            <div className='missao-container'>
-                <div className='missao'>
-                    <h3>Nossa Missão</h3>
-                    <p>Fazer da vida uma festa inesquecível. Na Stellaris, nossa missão é transformar ideias em experiências únicas - daquelas que viram boas histórias pra contar. Acreditamos no poder dos encontros, na energia dos eventos feitos com a alma e na magia de criar memórias que brilham mais que os flashes.</p>
-                    <h3>Nossos Valores</h3>
-                    <li>Experiência memorável - Criamos eventos que emocionam e conectam, garantindo momentos únicos para todos.</li>
-                    <li>Transparência e confiança - Priorizamos uma comunicação clara e aberta, essencial para parcerias e relacionamento com o público.</li>
-                    <li>Acessbilidade digital - Facilitamos o contato e participação via plataformas simples, como WhatsApp e formulários online.</li>
-                    <li>Segurança de dados - Garantimos a proteção das informações dos nossos clientes e parceiros em todas as interações.</li>
-                    <h3>O que oferecemos:</h3>
-                    <div className='oferecimentos'>
-                        <div className='parcerias'>
-                            <img src={iconHand}></img>
-                        </div>
-                        <div className='compra-ingressos'>
-                            <img src={iconMsg}></img>
-                        </div>
-                        <div className='compra-produtos'>
-                            <img src={iconBuy}></img>
-                        </div>
-                        <div className='avaliacao'>
-                            <img src={iconStar}></img>
-                        </div>
-                    </div>
+                <BotaoBranco texto={"Explorar Eventos"} onClick={null}/>
+        </div>
+         <div className='produtos-stellaris'>
+            <section>
+               <p className='title'>Produtos Stellaris</p>
+                <p className='sub-title'>Conheça os produtos oficiais!</p>
+                <BotaoBranco texto={"Ver catálogo completo"} onClick={null}/>
+            </section>
+        </div>
+        <div className='missao-container'>
+            <div className='missao'>
+            <h3>Nossa Missão</h3>
+            <p>Fazer da vida uma festa inesquecível. Na Stellaris, nossa missão é transformar ideias em experiências únicas - daquelas que viram boas histórias pra contar. Acreditamos no poder dos encontros, na energia dos eventos feitos com a alma e na magia de criar memórias que brilham mais que os flashes.</p>
+            <h3>Nossos Valores</h3>
+            <li>Experiência memorável - Criamos eventos que emocionam e conectam, garantindo momentos únicos para todos.</li>
+            <li>Transparência e confiança - Priorizamos uma comunicação clara e aberta, essencial para parcerias e relacionamento com o público.</li>
+            <li>Acessbilidade digital - Facilitamos o contato e participação via plataformas simples, como WhatsApp e formulários online.</li>
+            <li>Segurança de dados - Garantimos a proteção das informações dos nossos clientes e parceiros em todas as interações.</li>
+            <h3>O que oferecemos:</h3>
+            <div className='oferecimentos'>
+                <div className='parcerias'>
+                     <img src={iconHand}></img>
+                </div>
+                <div className='compra-ingressos'>
+                     <img src={iconMsg}></img>
+                </div>
+                <div className='compra-produtos'>
+                     <img src={iconBuy}></img>
+                </div>
+                <div className='avaliacao'>
+                     <img src={iconStar}></img>
+                </div>
                 </div>
             </div>
         </div>
+</div>
     )
 }
 
