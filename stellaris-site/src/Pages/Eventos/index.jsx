03bbe374b163b2f353f6a2f3405443803aee2f56
bbeatrizmedeiros <beatrizbemedeiros@alu.ufc.br>
2025-07-02 17:48:41 -0300

    pagina de eventos - ajustando o css

--- stellaris-site/src/Pages/Eventos/index.jsx
+++ stellaris-site/src/Pages/Eventos/index.jsx
@@ -13,31 +13,37 @@ export default function Eventos() {
             <div className="evento-futuro">
                 <h1>Bora pro Próximo? Garanta o Seu Ingresso!</h1>
                 <p>Em breve mais informações sobre o evento.</p>
-                <CardEventos
-                    titulo={"Tardezinha Stellaris - 2025"}
-                    descricao={"Em breve mais informações sobre o evento."}
-                    imagem={Tardezinha}
-                    link={"#"} />
+                <div className='card-futuro'>
+                    <CardEventos
+                        titulo={"Tardezinha Stellaris"}
+                        descricao={"Em breve!"}
+                        imagem={Tardezinha}
+                        link={"#"} />
+                </div>
+
             </div>
 
             <div className="eventos-passados">
                 <h1>Momentos Inesquecíveis</h1>
                 <p>Só quem viveu sabe... Mas dá pra sentir o clima aqui.</p>
-                <CardEventos 
-                    titulo={"Portal da Meia Noite - 2025"}
-                    descricao={"Finalizado com sucesso!"}
-                    imagem={PortalMeiaNoite}
-                    link={"#"}
-                />
+                <div className="card-passado">
+                    <CardEventos
+                        titulo={"Portal da Meia Noite (2025)"}
+                        descricao={"Finalizado com sucesso!"}
+                        imagem={PortalMeiaNoite}
+                        link={"#"}
+                    />
+                </div>
+
                 <div className="avaliacao-eventos">
                     <p>
-                    <strong>Avalie os eventos que você participou:</strong>{' '}
-                    <Link to="/avaliar">Clique aqui</Link>. <br />
-                    Eventos futuros terão opção de avaliação após a realização.
+                        <strong>Avalie os eventos que você participou:</strong>{' '}
+                        <Link to="/avaliar">Clique aqui</Link>. <br />
+                        Eventos futuros terão opção de avaliação após a realização.
                     </p>
                 </div>
             </div>
         </div>
-        
+
     );
 }
