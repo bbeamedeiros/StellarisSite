0b66dc2af97d1d5672f6c770ecf36c0c3ea73aee
yasminalmeidaa <yasminalmeidaa>
2025-07-10 18:48:29 -0300

    ajeitando um pouco o html

--- stellaris-site/src/Pages/Parceiros/index.jsx
+++ stellaris-site/src/Pages/Parceiros/index.jsx
@@ -35,25 +35,33 @@ export default function Parceiros() {
 
   return (
     <div className="parceiros-container">
+      <div>
       <h1>Vamos crescer juntos? <p></p> 
       Seja um parceiro da Stellaris!</h1>
+    
+      <br></br>
+
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br></br>
         Praesent ut malesuada nunc. <br></br>
         Curabitur vel lacus nec urna egestas aliquam. <br></br>
         Donec ac sem sed lorem commodo porttitor. <br></br>
       </p>
-      
+      </div>
+
       <br></br>
 
-      <p>
+      <div>
+      
         <h1>Tem interesse em se tornar <p></p> 
         um parceiro da Stellaris? </h1>
-      </p>
+      <br></br>
+
       <p>
         Clique abaixo e preencha nosso formulário de proposta.  <br></br>
         Vamos adorar conhecer sua ideia!
       </p>
+      </div>
     </div>
   );
 }
