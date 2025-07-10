703a91383d579c8489bd143fc6ab264d459779d6
yasminalmeidaa <yasminalmeidaa>
2025-07-10 18:38:33 -0300

    ajietando erro import De novo...

--- stellaris-site/src/Pages/Parceiros/index.jsx
+++ stellaris-site/src/Pages/Parceiros/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ref, onValue } from 'firebase/database';
-import { database } from '../../Firebase/firabase';
+import { database } from '../../Firebase/firebase';
 import './styles.css'
 
 export default function Parceiros() {
